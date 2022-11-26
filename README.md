f225e58a20bc2c939a21d88918de945d8330ff35
ben <ben@arc.wales>
2022-11-26 17:16:51 +0000

    Dropped creen support, refactored code
    INcluded better LED signalling

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 <img style="max-width:100%;" src="https://user-images.githubusercontent.com/33088785/166829474-a28ca2b7-dd3e-46d4-89d3-8a10bf1d3fad.png">
 
-## Compatible with any ESP32 microcontroller, with or without a screen, supporting native LN invoices and static LNURLPay links.
+## Compatible with any ESP32 microcontroller.
 
 For use with LNURLDevice in LNbits.
 
@@ -21,6 +21,8 @@ For use with LNURLDevice in LNbits.
 
 ### Flash and configure from browser! https://lnbits.github.io/bitcoinswitch/installer/
 
+> Press GPIO4 in first few seconds of boot to be able to config
+
 ### Complicated install instructions not using browser flashing
 - Install <a href="https://www.arduino.cc/en/software">Arduino IDE 1.8.19</a>
 - Install ESP32 boards, using <a href="https://docs.espressif.com/projects/arduino-esp32/en/latest/installing.html#installing-using-boards-manager">boards manager</a>
@@ -31,9 +33,5 @@ For use with LNURLDevice in LNbits.
 - Open this <a href="bitcoinSwitch.ino">bitcoinSwitch.ino</a> file in the Arduino IDE
 - Select the correct ESP32 board from tools>board
 - Upload to device
-
-> On M5Stack press A to launch portal within first few secs of startup, for all other ESP32 devices specify a GPIO to detect capacitance change, and touch within first few secs of startup.
-> Default password is "ToTheMoon"
-
 
 ![trigger](https://user-images.githubusercontent.com/33088785/166829947-d0194b32-19fc-4a16-83d3-dc6f9af9337c.gif)
--- bitcoinSwitch.ino
+++ bitcoinSwitch.ino
@@ -1,30 +1,17 @@
 #include <WiFi.h>
 #include <FS.h>
 #include <SPIFFS.h>
-#include <SPI.h>
-#include <TFT_eSPI.h>
-#include <Wire.h>
+#include <ArduinoJson.h>
+#include <WebSocketsClient.h>
 
 fs::SPIFFSFS &FlashFS = SPIFFS;
 #define FORMAT_ON_FAIL true
-#include <JC_Button.h>
-#include "qrcoded.h"
-
-#include <ArduinoJson.h>
-
-#include <WebSocketsClient.h>
-
 #define PARAM_FILE "/elements.json"
 
-/////////////////////////////////
 ///////////CHANGE////////////////
-/////////////////////////////////
-
-bool usingM5 = false; // false if not using M5Stack          
+         
 int portalPin = 4;
 
-/////////////////////////////////
-/////////////////////////////////
 /////////////////////////////////
 
 // Access point variables
@@ -38,18 +25,13 @@ String deviceId;
 String highPin;
 String timePin;
 String pinFlip;
-String lnurl;
 String dataId;
-String payReq;
-
-int balance;
-int oldBalance;
+String lnurl;
 
 bool paid;
 bool down = false;
-bool triggerUSB = false; 
+bool triggerConfig = false; 
 
-TFT_eSPI tft = TFT_eSPI();
 WebSocketsClient webSocket;
 
 struct KeyValue {
@@ -60,41 +42,25 @@ struct KeyValue {
 void setup()
 {
   Serial.begin(115200);
-  Wire.begin();
-  if(usingM5 == true){
-    tft.init();
-    tft.setRotation(1);
-    tft.invertDisplay(true);
-    logoScreen();
-  }
-  const byte BUTTON_PIN_A = 39;
-  Button BTNA(BUTTON_PIN_A);
-  BTNA.begin();
+
   int timer = 0;
   pinMode (2, OUTPUT);
 
   while (timer < 2000)
   {
     digitalWrite(2, LOW);
-    if (usingM5 == true){
-      if (BTNA.read() == 1){
-        Serial.println("Launch portal");
-        triggerUSB = true;
-        timer = 5000;
-      }
+    Serial.println(touchRead(portalPin));
+    if(touchRead(portalPin) < 40){
+      Serial.println("Launch portal");
+      triggerConfig = true;
+      timer = 5000;
     }
-    else{
-      Serial.println(touchRead(portalPin));
-      if(touchRead(portalPin) < 60){
-        Serial.println("Launch portal");
-        triggerUSB = true;
-        timer = 5000;
-      }
-    }
+    delay(150);
     digitalWrite(2, HIGH);
-    timer = timer + 100;
-    delay(300);
+    timer = timer + 300;
+    delay(150);
   }
+
   timer = 0;
 
   FlashFS.begin(FORMAT_ON_FAIL);
@@ -102,26 +68,28 @@ void setup()
   // get the saved details and store in global variables
   readFiles();
 
-  delay(2000);
-  
-  WiFi.begin(ssid.c_str(), wifiPassword.c_str());
-  while (WiFi.status() != WL_CONNECTED && timer < 8000) {
-    delay(1000);
-    Serial.print(".");
-    timer = timer + 1000;
-    if(timer > 7000){
-      triggerUSB = true;
+  if (triggerConfig == false){
+    WiFi.begin(ssid.c_str(), wifiPassword.c_str());
+    while (WiFi.status() != WL_CONNECTED && timer < 20000) {
+      delay(500);
+      digitalWrite(2, HIGH);
+      Serial.print(".");
+      timer = timer + 1000;
+      if(timer > 19000){
+        triggerConfig = true;
+      }
+      delay(500);
+      digitalWrite(2, LOW);
     }
   }
-  
-  if (triggerUSB == true)
+
+  if (triggerConfig == true)
   {
-    Serial.println("USB triggered");
+    digitalWrite(2, HIGH);
+    Serial.println("USB Config triggered");
     configOverSerialPort();
   }
 
-  lnbitsScreen();
-  delay(1000);
   pinMode(highPin.toInt(), OUTPUT);
   onOff();
   Serial.println(lnbitsServer + "/lnurldevice/ws/" + deviceId);
@@ -131,70 +99,23 @@ void setup()
 
 void loop() {
   while(WiFi.status() != WL_CONNECTED){
-    if(usingM5 == true){
-      connectionError();
-    }
     Serial.println("Failed to connect");
     delay(500);
   }
-  Serial.println(highPin.toInt());
-
-  if(lnurl == "true"){
-    if(usingM5 == true){
-      qrdisplayScreen();
-    }
-    paid = false;
-    while(paid == false){
-      webSocket.loop();
-      if(paid){
-        Serial.println(payloadStr);
-        highPin = getValue(payloadStr, '-', 0);
-        Serial.println(highPin);
-        timePin = getValue(payloadStr, '-', 1);
-        Serial.println(timePin);
-        if(usingM5 == true){
-          completeScreen();
-        }
-        onOff();
-      }
-    }
-    Serial.println("Paid");
-    if(usingM5 == true){
-      paidScreen();
+  digitalWrite(2, LOW);
+  paid = false;
+  while(paid == false){
+    webSocket.loop();
+    if(paid){
+      Serial.println(payloadStr);
+      highPin = getValue(payloadStr, '-', 0);
+      Serial.println(highPin);
+      timePin = getValue(payloadStr, '-', 1);
+      Serial.println(timePin);
+      onOff();
     }
   }
-  else{
-    getInvoice();
-    if(down){
-      errorScreen();
-      getInvoice();
-      delay(5000);
-    }
-    if(payReq != ""){
-      if(usingM5 == true){
-        qrdisplayScreen();
-      }
-      delay(5000);
-    }
-    while(paid == false && payReq != ""){
-      webSocket.loop();
-      if(paid){
-        Serial.println(payloadStr);
-        highPin = getValue(payloadStr, '-', 0);
-        Serial.println(highPin);
-        timePin = getValue(payloadStr, '-', 1);
-        Serial.println(timePin);
-        if(usingM5 == true){
-          completeScreen();
-        }
-        onOff();
-      }
-    }
-    payReq = "";
-    dataId = "";
-    paid = false;
-    delay(4000);
-  }
+  Serial.println("Paid");
 }
 
 //////////////////HELPERS///////////////////
@@ -269,139 +190,6 @@ void readFiles()
   paramFile.close();
 }
 
-//////////////////DISPLAY///////////////////
-
-void serverError()
-{
-  tft.fillScreen(TFT_WHITE);
-  tft.setCursor(0, 80);
-  tft.setTextSize(3);
-  tft.setTextColor(TFT_RED);
-  tft.println("Server connect fail");
-}
-
-void connectionError()
-{
-  tft.fillScreen(TFT_WHITE);
-  tft.setCursor(0, 80);
-  tft.setTextSize(3);
-  tft.setTextColor(TFT_RED);
-  tft.println("Wifi connect fail");
-}
-
-void connection()
-{
-  tft.fillScreen(TFT_WHITE);
-  tft.setCursor(0, 80);
-  tft.setTextSize(3);
-  tft.setTextColor(TFT_RED);
-  tft.println("Wifi connected");
-}
-
-void logoScreen()
-{ 
-  tft.fillScreen(TFT_WHITE);
-  tft.setCursor(0, 80);
-  tft.setTextSize(4);
-  tft.setTextColor(TFT_PURPLE);
-  tft.println("bitcoinSwitch");
-}
-
-void portalLaunched()
-{ 
-  tft.fillScreen(TFT_WHITE);
-  tft.setCursor(0, 80);
-  tft.setTextSize(4);
-  tft.setTextColor(TFT_PURPLE);
-  tft.println("PORTAL LAUNCH");
-}
-
-void processingScreen()
-{ 
-  tft.fillScreen(TFT_BLACK);
-  tft.setCursor(40, 80);
-  tft.setTextSize(4);
-  tft.setTextColor(TFT_WHITE);
-  tft.println("PROCESSING");
-}
-
-void lnbitsScreen()
-{ 
-  tft.fillScreen(TFT_WHITE);
-  tft.setCursor(10, 90);
-  tft.setTextSize(3);
-  tft.setTextColor(TFT_BLACK);
-  tft.println("POWERED BY LNBITS");
-}
-
-void portalScreen()
-{ 
-  tft.fillScreen(TFT_BLACK);
-  tft.setCursor(30, 80);
-  tft.setTextSize(3);
-  tft.setTextColor(TFT_WHITE);
-  tft.println("PORTAL LAUNCHED");
-}
-
-void paidScreen()
-{ 
-  tft.fillScreen(TFT_BLACK);
-  tft.setCursor(110, 80);
-  tft.setTextSize(4);
-  tft.setTextColor(TFT_WHITE);
-  tft.println("PAID");
-}
-
-void completeScreen()
-{ 
-  tft.fillScreen(TFT_BLACK);
-  tft.setCursor(60, 80);
-  tft.setTextSize(4);
-  tft.setTextColor(TFT_WHITE);
-  tft.println("COMPLETE");
-}
-
-void errorScreen()
-{ 
-  tft.fillScreen(TFT_BLACK);
-  tft.setCursor(70, 80);
-  tft.setTextSize(4);
-  tft.setTextColor(TFT_WHITE);
-  tft.println("ERROR");
-}
-
-void qrdisplayScreen()
-{
-  String qrCodeData;
-  if(lnurl == "true"){
-    qrCodeData = lnurl;
-  }
-  else{
-    qrCodeData = payReq;
-  }
-  tft.fillScreen(TFT_WHITE);
-  qrCodeData.toUpperCase();
-  const char *qrDataChar = qrCodeData.c_str();
-  QRCode qrcoded;
-  uint8_t qrcodeData[qrcode_getBufferSize(20)];
-  qrcode_initText(&qrcoded, qrcodeData, 11, 0, qrDataChar);
-  for (uint8_t y = 0; y < qrcoded.size; y++)
-  {
-    // Each horizontal module
-    for (uint8_t x = 0; x < qrcoded.size; x++)
-    {
-      if (qrcode_getModule(&qrcoded, x, y))
-      {
-        tft.fillRect(65 + 3 * x, 20 + 3 * y, 3, 3, TFT_BLACK);
-      }
-      else
-      {
-        tft.fillRect(65 + 3 * x, 20 + 3 * y, 3, 3, TFT_WHITE);
-      }
-    }
-  }
-}
-
 //////////////////NODE CALLS///////////////////
 
 void checkConnection(){
@@ -410,90 +198,11 @@ void checkConnection(){
   const char* lnbitsserver = lnbitsServer.c_str();
   if (!client.connect(lnbitsserver, 443)){
     down = true;
-    serverError();
     return;   
   }
 }
 
-void getInvoice(){
-  WiFiClientSecure client;
-  client.setInsecure();
-  const char* lnbitsserver = lnbitsServer.c_str();
-  if (!client.connect(lnbitsserver, 443)){
-    down = true;
-    return;   
-  }
-  StaticJsonDocument<500> doc;
-  DeserializationError error;
-  char c;
-  String line;
-  String url = "/lnurldevice/api/v1/lnurl/";
-  client.print(String("GET ") + url + deviceId +" HTTP/1.1\r\n" +
-                "Host: " + lnbitsserver + "\r\n" +
-                "User-Agent: ESP32\r\n" +
-                "Content-Type: application/json\r\n" +
-                "Connection: close\r\n\r\n");
-  while (client.connected()) {
-    line = client.readStringUntil('\n');
-    if (line == "\r") {
-      break;
-    }
-  }
-  String callback;
-  while (client.available()) {
-    line = client.readStringUntil('\n');
-    callback = line;
-  }
-  Serial.println(callback);
-  delay(500);
-  error = deserializeJson(doc, callback);
-  if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
-    return;
-  }
-  const char* callbackChar = doc["callback"];
-  String callbackStr = callbackChar;
-  getCallback(callbackStr);
-}
-
-void getCallback(String callbackStr){
-  WiFiClientSecure client;
-  client.setInsecure();
-  const char* lnbitsserver = lnbitsServer.c_str();
-  if (!client.connect(lnbitsserver, 443)){
-    down = true;
-    return;   
-  }
-  StaticJsonDocument<500> doc;
-  DeserializationError error;
-  char c;
-  String line;
-  client.print(String("GET ") + callbackStr.substring(8 + lnbitsServer.length()) +" HTTP/1.1\r\n" +
-                "Host: " + lnbitsserver + "\r\n" +
-                "User-Agent: ESP32\r\n" +
-                "Content-Type: application/json\r\n" +
-                "Connection: close\r\n\r\n");
-   while (client.connected()) {
-   String line = client.readStringUntil('\n');
-   Serial.println(line);
-    if (line == "\r") {
-      break;
-    }
-  }
-  line = client.readString();
-  Serial.println(line);
-  error = deserializeJson(doc, line);
-  if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
-    return;
-  }
-  const char* temp = doc["pr"];
-  payReq = temp;
-}
 //////////////////WEBSOCKET///////////////////
-
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
     switch(type) {
@@ -512,7 +221,6 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
         case WStype_TEXT:
             payloadStr = (char*)payload;
             paid = true;
-            
 		case WStype_ERROR:			
 		case WStype_FRAGMENT_TEXT_START:
 		case WStype_FRAGMENT_BIN_START:
@@ -520,5 +228,4 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 		case WStype_FRAGMENT_FIN:
 			break;
     }
-
 }
