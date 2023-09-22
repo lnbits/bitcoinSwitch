add6644a9ac216877f9d9fbef5973f89f96ca763
dni ⚡ <office@dnilabs.com>
2023-09-22 12:49:27 +0200

    fix builder for pages

--- build-webinstaller.sh
+++ build-webinstaller.sh
@@ -7,10 +7,14 @@ mkdir -p ./installer/firmware/esp32
 
 for version in $(jq -r '.versions[]' ./installer/versions.json); do
     mkdir -p ./installer/firmware/esp32/$version
-    cd ./installer/firmware/esp32/$version
     wget https://github.com/lnbits/bitcoinswitch/releases/download/$version/manifest.json
+    mv manifest.json ./installer/firmware/esp32/$version
     wget https://github.com/lnbits/bitcoinswitch/releases/download/$version/bootloader.bin
+    mv bootloader.bin ./installer/firmware/esp32/$version
     wget https://github.com/lnbits/bitcoinswitch/releases/download/$version/boot_app0.bin
+    mv boot_app0.bin ./installer/firmware/esp32/$version
     wget https://github.com/lnbits/bitcoinswitch/releases/download/$version/bitcoinSwitch.ino.bin
+    mv bitcoinSwitch.ino.bin ./installer/firmware/esp32/$version
     wget https://github.com/lnbits/bitcoinswitch/releases/download/$version/bitcoinSwitch.ino.partitions.bin
+    mv bitcoinSwitch.ino.partitions.bin ./installer/firmware/esp32/$version
 done
