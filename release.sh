e7b58524fe9a94e28c5fe985959a5edd41642d92
dni ⚡ <office@dnilabs.com>
2023-09-24 20:42:01 +0200

    [FEAT] move build files to build-template

--- release.sh
+++ release.sh
@@ -6,8 +6,8 @@ mkdir -p dist/$tag
 
 cp build/bitcoinSwitch.ino.bin dist/$tag
 cp build/bitcoinSwitch.ino.partitions.bin dist/$tag
-cp build/boot_app0.bin dist/$tag
-cp build/bootloader.bin dist/$tag
+cp build-template/boot_app0.bin dist/$tag
+cp build-template/bootloader.bin dist/$tag
 
-cp build/manifest.template.json dist/$tag/manifest.json
+cp build-template/manifest.template.json dist/$tag/manifest.json
 sed -i -e "s/%version%/$tag/g" dist/$tag/manifest.json
