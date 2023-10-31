3b60f46986bd7e868542d9c4c890408499e410e3
dni ⚡ <office@dnilabs.com>
2023-10-31 14:35:24 +0100

    feat: new `hardware-installer` (#40)

---
+++ INSTALLER.md
@@ -0,0 +1,18 @@
+# bitcoinSwitch
+## Turn things on with bitcoin!
+
+<img style="max-width:100%;" src="https://user-images.githubusercontent.com/33088785/166832680-600ed270-cbc9-4749-82f1-c1853b242329.png">
+<img style="max-width:100%;" src="https://user-images.githubusercontent.com/33088785/166829474-a28ca2b7-dd3e-46d4-89d3-8a10bf1d3fad.png">
+
+### Flash and configure from browser!
+
+> Once flashed, press GPIO4 in few seconds of ESP32 booting up to be able to config
+
+   <table>
+  <tr>
+    <th><img src="https://user-images.githubusercontent.com/33088785/204107016-bc9473e0-2843-4873-af71-cd934e07f444.gif" alt="Snow" style="width:100%"></th>
+    <th><img src="https://user-images.githubusercontent.com/33088785/204107029-cc4ad95b-b130-4b48-9091-86d7be7d4f16.gif" alt="Forest" style="width:100%"></th>
+    <th><img src="https://user-images.githubusercontent.com/33088785/204107037-870571f8-b860-4019-93d4-bbdbeaf1091f.gif" alt="Mountains" style="width:100%"></th>
+    <th><img src="https://user-images.githubusercontent.com/33088785/204107044-b8a7d94f-6908-40dd-bb82-974e08f077f4.gif" alt="Mountains" style="width:100%"></th>
+  </tr>
+</table>
---
+++ config.js
@@ -0,0 +1,72 @@
+export const addressesAndFiles = [
+  {
+    address: "0x1000",
+    fileName: "bitcoinSwitch.ino.bootloader.bin",
+  },
+  {
+    address: "0x8000",
+    fileName: "bitcoinSwitch.ino.partitions.bin",
+  },
+  {
+    address: "0xE000",
+    fileName: "boot_app0.bin",
+  },
+  {
+    address: "0x10000",
+    fileName: "bitcoinSwitch.ino.bin",
+  },
+];
+
+export const configPath = "elements.json";
+
+export const elements = [
+  {
+    name: "password",
+    value: "ToTheMoon1",
+    label: "Password for PoS AP WiFi",
+    type: "text",
+  },
+  {
+    name: "ssid",
+    value: "",
+    label: "WiFi SSID",
+    type: "text",
+  },
+  {
+    name: "wifipassword",
+    value: "",
+    label: "WiFi Password",
+    type: "text",
+  },
+  {
+    name: "socket",
+    value: "",
+    label: "LNURLDEVICE SWITCH OPTION Use LNURLDevices extension in LNbits",
+    type: "text",
+  },
+  {
+    name: "wallet",
+    value: "",
+    label:
+      "THRESHOLD OPTION (use lnurlp extension to make an lnurlp) wallet ID from LNbits",
+    type: "text",
+  },
+  {
+    name: "threshold",
+    value: "",
+    label: "THRESHOLD OPTION amount to hit to trigger",
+    type: "text",
+  },
+  {
+    name: "thresholdPin",
+    value: "",
+    label: "THRESHOLD OPTION pin to trigger",
+    type: "text",
+  },
+  {
+    name: "thresholdTime",
+    value: "",
+    label: "THRESHOLD OPTION time to turn of for",
+    type: "text",
+  },
+];
